78af7d73952d3843b381a2e8ef8f39cac2e7397d
BhavanaM19 <76389447+BhavanaM19@users.noreply.github.com>
2023-10-05 14:40:20 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,13 @@
+print("Demo of basic data types:String")
+s="hello"
+t="world"
+print("String1:",s)
+print("String2:",t)
+d=s+" "+t
+print("String Concatenation:",d)
+print("Capitalize:",d.capitalize())
+print("Converted Uppercase:",s.upper())
+print("Right justify a string:",s.rjust(7))
+print("String at center:",s.center(7))
+print("After replacing| with ell:",s.replace('1','(ell)'))
+print("String after stripping leading and trailing white spaces:",'word'.strip())

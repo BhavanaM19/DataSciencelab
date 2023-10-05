7c7b1ed284e5c07c0cd567183f5cd21bf7e842c2
BhavanaM19 <76389447+BhavanaM19@users.noreply.github.com>
2023-10-05 14:06:48 +0530

    Add files via upload

---
+++ bool.py
@@ -0,0 +1,10 @@
+print ("Demo of basic data types:Boolean")
+t=True
+f=False
+print("t=",t)
+print("f=",f)
+print("data type of variable t:",type(t))
+print("data type of variable f:",type(f))
+print("logical AND operation:",t and f)
+print("logical OR operation:",t or f)
+print(" logical NOT operation::",t!=f)
